df421778e06adacbce9bb995e76e1ec53253d6cd
strawhatrag <sreeragbharathan791@gmail.com>
2024-01-06 16:40:33 +0530

    outlet added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import React from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
-import Directory from "./directory/directory.component";
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
-  return <Directory categories={categories} />;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
