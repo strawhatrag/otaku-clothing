2d51b249c4f93cd78a31ec6fdc411eb6b6ed04b7
strawhatrag <sreeragbharathan791@gmail.com>
2024-02-04 16:19:13 +0530

    cart toggle activated

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,16 @@
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
-
-
+import { useContext } from 'react';
+import { CartContext } from '../context/cart.context';
 
 
 const CartIcon = () => {
+  const {isCartOpen, setIsCartOpen} = useContext(CartContext)
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container'>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
     <ShoppingIcon className='shopping-icon'></ShoppingIcon>
     <span className='item-count'>0</span>
     </div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,9 +9,14 @@ import { ReactComponent as OtakuLogo } from "../../assets/logo.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { CartContext } from "../../components/context/cart.context";
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  //console.log(currentUser);
+ const {isCartOpen} = useContext(CartContext);
 
  
   return (
@@ -36,6 +41,7 @@ const Navigation = () => {
           )}
           <CartIcon></CartIcon>
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
