c2a73586fb5572144582fa3724db1e3199e51846
strawhatrag <sreeragbharathan791@gmail.com>
2024-02-08 17:11:11 +0530

    fixed button size

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,24 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
 
+import {useContext} from 'react';
+import { CartContext } from '../context/cart.context';
+
 
 import CartItem from '../cart-item/cart-item.component'
 
 
 const CartDropdown = () =>{
+
+  const {cartItems} = useContext(CartContext)
   return (<div className='cart-dropdown-container'>
     <div className='cart-items'>
-      {[].map(item => <CartItem cartItem={item} />)}
-    <Button>GO TO CHECKOUT</Button>
+      {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+    
     </div>
+    <Button>GO TO CHECKOUT</Button>
   </div>)
-}
+} 
 
 
 export default CartDropdown;
